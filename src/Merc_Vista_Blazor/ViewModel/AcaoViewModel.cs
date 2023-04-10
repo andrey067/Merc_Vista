115c57c34b5b6b47671b66981dc1b7ba2acf650c
audrey.lima <audrey.lima@ms.senac.br>
2023-04-10 16:38:11 -0400

    Ajustes no carregamento dos arquivos

---
+++ src/Merc_Vista_Blazor/ViewModel/AcaoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Merc_Vista_Blazor.ViewModel
+{
+    public record AcaoViewModel(long Id, 
+                                string Ativo, 
+                                DateTime? Data, 
+                                decimal Abertura, 
+                                decimal Maximo, 
+                                decimal Minimo, 
+                                decimal Fechamento, 
+                                decimal Volume, 
+                                decimal Quantidade);
+
+}
